c117a2e44459d2f1ce27a72473f397ac896997e0
sophialuanfx <31905733+sophialuanfx@users.noreply.github.com>
2020-10-23 09:07:56 -0400

    Add files via upload

---
+++ otherProfile.js
@@ -0,0 +1,20 @@
+ let users = require("./users.json");
+ let movie = require("./movie-data-short.json");
+
+ function makeFollow(userA, userB){
+    //If one of the user IDs doesn't exist, stop
+    if(!users.hasOwnProperty(userA) && !users.hasOwnProperty(userB)){
+      return;
+    }
+  
+    //If the users are already friends, stop
+    if(users[userA].following.includes(userB)){
+      return;
+    }
+  
+    //Update both so they are now friends
+    users[userA].following.push(userB);
+    users[userB].following.push(userA);
+  }
+
+  
