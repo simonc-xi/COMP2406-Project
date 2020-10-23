b3cee1daecfd7093e2cf24be77642773f97fefe8
sophialuanfx <sophialuanfx@gmail.com>
2020-10-23 19:03:54 -0400

    update

--- movie.js
+++ movie.js
@@ -1,31 +1,6 @@
 let movies = require ("./movie-data-short.jason");
 let users = require("./users.json");
 
-function searchUsers(requestingUser, searchTerm){
-    let results = [];
-  
-    //If the user is not valid, return an empty array.
-    //You could return null to indicate an error or any other value to signify the requesting user was not valid.
-    if(!isValidUser(requestingUser)){
-      return results;
-    }
-  
-    //If users was an array, you could use a nice one line filter function call
-    for(username in users){
-      let user = users[username];
-      //If this user matches the search term
-      if(user.username.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0){
-        //If the requesting user is allowed to access the matching user
-        if(user.username === requestingUser.username || requestingUser.friends.includes(user.username)){
-          results.push(user);
-        }
-      }
-    }
-  
-    return results;
-  }
-
-
   
   function createReview(requestingUser, movieTitle, newR){
     //We should verify the contents of the question and we should verify the user
--- searchBar.js
+++ searchBar.js
@@ -1,6 +1,17 @@
 let users = require("./users.json");
 let movies = require("./movie-data-short.json");
 
+
+function isValidUser(userObj){
+  if(!userObj){
+    return false;
+  }
+  if(!userObj.username || !users.hasOwnProperty(userObj.username)){
+    return false;
+  }
+  return true;
+}
+
 //search the user
 function searchUsers(requestingUser, searchTerm){
     let results = [];
@@ -26,7 +37,8 @@ function searchUsers(requestingUser, searchTerm){
     return results;
   }
 
-  function searchMovie(movieName, searchTerm){
+  //search Movie Title
+  function searchMovie(movieName){
     let results = [];
   
     //If the user is not valid, return an empty array.
@@ -36,42 +48,52 @@ function searchUsers(requestingUser, searchTerm){
       return results;
     }*/
   
-    //If users was an array, you could use a nice one line filter function call
     for(Title in movies){
-      let user = users[username];
-      //If this user matches the search term
-      if(user.username.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0){
-        //If the requesting user is allowed to access the matching user
-        if(user.username === requestingUser.username || requestingUser.friends.includes(user.username)){
-          results.push(user);
-        }
+      let movie = movies[Title];
+        if(movie.keywords.includes(movieName)){
+          results.push(movie);
+       
       }
     }
   
     return results;
   }
 
-  function searchPeople(peopleName, searchTerm){
+  //search people (writer, actor, director)
+  function searchPeople(peopleName){
     let results = [];
-  
-    //If the user is not valid, return an empty array.
-    //You could return null to indicate an error or any other value to signify the requesting user was not valid.
+
     /*
     if(!isValidUser(requestingUser)){
       return results;
     }*/
-  
-    //If users was an array, you could use a nice one line filter function call
-    for(username in users){
-      let user = users[username];
-      //If this user matches the search term
-      if(user.username.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0){
-        //If the requesting user is allowed to access the matching user
-        if(user.username === requestingUser.username || requestingUser.friends.includes(user.username)){
-          results.push(user);
+
+    for(Writer in movies){
+      let movieWriter = movies[Writer];
+        if(movieWriter.keywords.includes(peopleName)){
+          results.push(movieWriter);
         }
       }
-    }
-  
+    
+    for(Actors in movies){
+      let movieActors = movies[Actors];
+        if(movieActors.keywords.includes(peopleName)){
+          results.push(movieActors);
+        }
+      }
+
+    for(Director in movies){
+      let movieDirector = movies[Director];
+        if(movieDirector.keywords.includes(peopleName)){
+          results.push(movieDirector);
+        }
+      }
+
     return results;
   }
+
+//check
+console.log("Testing searchUsers");
+let result = searchUsers(users.user0, "Sophia")
+console.log(result);
+//assert(result.length === 2);
