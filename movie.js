86535c99ee6fc8c377d447008f6199afc565c0ec
sophialuanfx <sophialuanfx@gmail.com>
2020-10-23 20:59:09 -0400

    Update movie.js

--- movie.js
+++ movie.js
@@ -5,19 +5,13 @@ let users = require("./users.json");
 function createQReview(requestingUser, newR, title){
   //We should verify the contents of the question and we should verify the user
   //We will skip validating the question for now (it's almost dinner time).
-  /*
-  if(!isValidUser(requestingUser)){
-    return null;
-  }*/
-
-  //Title = String(nextQuestionID);
-  //newQ.creator = requestingUser.username;
-  //nextQuestionID++;
-
-  requestingUser.reviews[1].push(newR);
-  users[requestingUser.username].reviews.title.push(title);
-  return newR;
+  for (i in users){
+    for (j in users[i].reviews){
+    
+    }
+  }
 }
+
 
 
 console.log("add a new review");
