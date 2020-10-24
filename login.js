9ffe4c131b6c26b05756e451e9c6d1a595e72973
sophialuanfx <sophialuanfx@gmail.com>
2020-10-23 23:02:41 -0400

    update

--- login.js
+++ login.js
@@ -1,28 +1,28 @@
 //let users = require("./users.json");
 
 //check the user is already valid form or not.
-function isValidUser(userObj){
-  if(!userObj){
+function isValidUser(userOb){
+  if(!userOb){
     return false;
   }
-  if(!userObj.username || !users.hasOwnProperty(userObj.username)){
+  if(!userOb.username || !users.hasOwnProperty(userOb.username)){
     return false;
   }
   return true;
 }
 
-function getUser(requestingUser, userID){
+function getUser(requestingUser, userNum){
     //If the requesting user is invalid (e.g., is not logged in, is missing username, anything else expected is invalid), disallow
     if(!isValidUser(requestingUser)){
       return null;
     }
 
     //If the requested userID exists and the requesting user is allowed to access it, return the user
-    if(users.hasOwnProperty(userID)){
+    if(users.hasOwnProperty(userNum)){
       //It may be beneficial to create a helper function for canAccessUser(requesterID, requestedID)
       //You could use this function in searchUsers below too
-      if(requestingUser.username == userID){
-        return users[userID];
+      if(requestingUser.username == userNum){
+        return users[userNum];
       }
     }
 
